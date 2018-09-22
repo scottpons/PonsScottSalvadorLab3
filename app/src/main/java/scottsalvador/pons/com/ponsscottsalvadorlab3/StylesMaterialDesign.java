31099ea777f256e9a1e0f32c51a270f82f35d113
scottpons <scottpons09@gmail.com>
2018-09-22 22:40:47 +0800

    Initial commit

---
+++ app/src/main/java/scottsalvador/pons/com/ponsscottsalvadorlab3/StylesMaterialDesign.java
@@ -0,0 +1,13 @@
+package scottsalvador.pons.com.ponsscottsalvadorlab3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StylesMaterialDesign extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraint);
+    }
+}
